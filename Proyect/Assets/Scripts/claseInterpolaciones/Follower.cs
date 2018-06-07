be46f9d0ddeae3703b9d4defef6bbe3a7b1c62d0
Unknown <ale.bros@hotmail.com>
2018-06-07 11:41:28 -0300

    Add Scripts from Interpolation Class 06/06

---
+++ Proyect/Assets/Scripts/claseInterpolaciones/Follower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour 
+{
+	[SerializeField] Transform target;
+	[SerializeField] float speed;
+
+	void Update()
+	{
+		var diff = target.position - transform.position;
+		transform.position += diff * speed;
+	}
+}
---
+++ Proyect/Assets/Scripts/claseInterpolaciones/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	[SerializeField] float speed;
+
+	void Update()
+	{
+		var movDir = Vector3.zero;
+		movDir += transform.forward * Input.GetAxis ("Vertical");
+		movDir += transform.right * Input.GetAxis ("Horizontal");
+		movDir = Vector3.ClampMagnitude (movDir, 1);
+
+		var mov = Vector3.zero;
+		mov += movDir * speed;
+		//TODO: Agregar fuerza de salto y gravedad
+		transform.position += mov * Time.deltaTime;
+	}
+}
